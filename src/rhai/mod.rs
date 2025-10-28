1de9476f5da6aceb1c7359f757a6b46427b8c8a1
Chiffa <artemy040404@gmail.com>
2025-10-28 10:30:49 +0300

    feat: in-place ordering function
    You kinda can't write anything meaningful without one, can you?

--- src/rhai/mod.rs
+++ src/rhai/mod.rs
@@ -1,3 +1,34 @@
-use rhai::{Array, Dynamic, FnPtr};
+use rhai::{Array, Dynamic, FnPtr, NativeCallContext};
 
 pub mod osu;
+
+/// Define an in-place sort
+pub fn order(ctx: NativeCallContext, mut array: Vec<Dynamic>, comparer: FnPtr) -> Vec<Dynamic> {
+    if array.len() <= 1 {
+        return array;
+    }
+    array.sort_by(|x, y| {
+        comparer
+            .call_raw(&ctx, None, [x.clone(), y.clone()])
+            .ok()
+            .and_then(|v| {
+                v.as_int()
+                    .or_else(|_| v.as_bool().map(|v| if v { -1 } else { 1 }))
+                    .ok()
+            })
+            .map_or_else(
+                || x.type_id().cmp(&y.type_id()),
+                |v| match v {
+                    v if v > 0 => std::cmp::Ordering::Greater,
+                    v if v < 0 => std::cmp::Ordering::Less,
+                    0 => std::cmp::Ordering::Equal,
+                    _ => unreachable!("v is {}", v),
+                },
+            )
+    });
+    array
+}
+
+pub fn register_helpers(engine: &mut rhai::Engine) -> &mut rhai::Engine {
+    engine.register_fn("order", order)
+}
